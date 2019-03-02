8ab15aa37724aed817f2fbbb77dc9974c354ee93
Nabil Galeb <nabilgaleb@hotmail.com>
2019-03-02 10:07:05 -0400

    Segundo Commit

---
+++ SolAritmetica/WcfAritmetica/IWSaritmetica.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfAritmetica
+{
+    
+    public interface IWSaritmetica
+    {
+        [OperationContract]
+        clsAritmetica sumar(int numero1, int numero2);
+
+    }
+    [DataContract]
+    public class clsAritmetica
+    {
+        [DataMember]
+        public int numeroA { get; set; }
+        public int numeroB { get; set; }
+        public int respuesta { get; set; }
+    }
+
+
